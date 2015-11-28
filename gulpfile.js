e2cfa23a2d5f9f98242751fa6a3f87ae2889b440
Fynn Becker <info@fynn-becker.de>
2015-11-28 11:38:48 +0100

    More pretty console log

--- gulpfile.js
+++ gulpfile.js
@@ -10,6 +10,7 @@ var del          = require('del');
 var gulp         = require('gulp');
 var rename       = require('gulp-rename');
 var concat       = require('gulp-concat');
+var colors       = require('colors');
 
 // CSS/Sass
 var sass         = require('gulp-sass');
@@ -45,7 +46,6 @@ var config = {
   sassDist:    path.dist + 'css',
 
   // JavaScript
-  //jsInput:     path.src  + 'js/main.js',
   jsInput:     path.src  + 'js/**/*.js',
   jsWatch:     path.src  + 'js/**/*.js',
   jsDev:       path.dev  + 'js',
@@ -53,7 +53,7 @@ var config = {
 
   // Assets
   assetsInput: path.src  + 'assets/**/*',
-  assestWatch: path.src  + 'assets/**/*',
+  assetsWatch: path.src  + 'assets/**/*.*',
   assetsDev:   path.dev  + 'assets',
   assetsDist:  path.dist + 'assets'
 }
@@ -74,12 +74,6 @@ gulp.task('clean:dev', function() {
 // Clean Distribution
 gulp.task('clean:dist', function() {
   return del(path.dist);
-});
-
-
-// Clean Assets
-gulp.task('clean:assets', function() {
-  return del(config.assetsDev);
 });
 
 
@@ -149,6 +143,12 @@ gulp.task('js:dist', function() {
  * ========================================================================== */
 
 
+// Remove Assets folder
+gulp.task('assets:clean', function() {
+  return del(config.assetsDev);
+});
+
+
 // Assets Development
 gulp.task('assets:dev', ['assets:clean'], function() {
   return gulp.src(config.assetsInput)
@@ -160,4 +160,64 @@ gulp.task('assets:dev', ['assets:clean'], function() {
 gulp.task('assets:dist', function() {
   return gulp.src(config.assetsInput)
     .pipe(gulp.dest(config.assetsDist));
+});
+
+
+
+
+/* DISTRIBUTION
+ * Run alle the *:dist tasks
+ * ========================================================================== */
+
+
+gulp.task('distribution', ['clean:dist'], function() {
+  gulp.run('sass:dist');
+});
+
+
+
+
+/* DEFAULT
+ * Run alle the *:dev tasks and watch
+ * ========================================================================== */
+
+
+gulp.task('default', ['sass:dev', 'js:dev', 'assets:dev'], function() {
+  var watchSass   = gulp.watch(config.sassWatch, ['sass:dev']);
+  var watchJS     = gulp.watch(config.jsWatch, ['js:dev']);
+  var watchAssets = gulp.watch(config.assetsWatch, ['assets:dev']);
+
+  /**
+   * Return the current time with fancy formatting
+   */
+  var outputTime = function() {
+    var date = new Date();
+    var time = ('0' + date.getUTCHours()).slice(-2) + ':' +
+               ('0' + date.getUTCMinutes()).slice(-2) + ':' +
+               ('0' + date.getUTCSeconds()).slice(-2);
+
+    return '\n[' + time.gray + '] ';
+  };
+
+  /**
+   * Return a message based on input
+   */
+  var outputMsg = function(event) {
+    return 'File ' + event.path.magenta + ' was ' + event.type;
+  };
+
+  // Sass Notification
+  watchSass.on('change', function(e) {
+    console.log(outputTime() + outputMsg(e));
+  });
+
+  // JS Notification
+  watchJS.on('change', function(e) {
+    console.log(outputTime() + outputMsg(e));
+  });
+
+  // Assets Notification
+  watchAssets.on('change', function(e) {
+    console.log(outputTime() + outputMsg(e));
+  });
 });
