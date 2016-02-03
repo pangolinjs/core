8345e0148d6b7c9f0d03c99c4ab5fcb910f271db
Fynn Becker <info@fynn-becker.de>
2016-02-03 12:22:46 +0100

    Try out gulp-responsive

--- gulpfile.js
+++ gulpfile.js
@@ -36,6 +36,7 @@ var frontMatter  = require('gulp-front-matter');
 
 // Assets
 var imagemin     = require('gulp-imagemin');
+var responsive   = require('gulp-responsive');
 
 // BrowserSync
 var browserSync  = require('browser-sync');
@@ -138,6 +139,25 @@ var config = {
       progressive: true,
       interlaced: true,
       multipass: true
+    },
+    responsive: {
+      images: {
+        '*': [{
+          width: '100%',
+          rename: {suffix: '@3x'}
+        }, {
+          width: '50%',
+          rename: {suffix: '@2x'}
+        }, {
+          width: '33.33333333333%',
+          rename: {suffix: '@1x'}
+        }]
+      },
+      options: {
+        quality: 100,
+        withMetadata: false,
+        interpolation: 'nohalo'
+      }
     }
   }
 }
@@ -407,6 +427,7 @@ var assetsDev = function() {
 
   log.activity('Copying assets...');
   gulp.src(paths.assets.src + '**')
+    .pipe(responsive(config.assets.responsive.images, config.assets.responsive.options))
     .pipe(gulp.dest(paths.assets.dev));
   log.activity('Finished copying.');
 };
