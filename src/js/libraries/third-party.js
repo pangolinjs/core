bf50a0b94c65bbf0615094e94480c64fce0c4223
Fynn Becker <info@fynn-becker.de>
2016-07-10 09:33:33 +0200

    Add SVG for Everybody

--- src/js/libraries/third-party.js
+++
@@ -1,7 +0,0 @@
-/*
- * Use the js/libraries folder for third-party stuff.
-*/
-
-// Console Logging triggers an ESLint error,
-// but files within this folder will not be linted.
-console.log('Third Party Library v0.0.0');
