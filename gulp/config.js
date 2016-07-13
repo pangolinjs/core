15a47042c1472e29daf65934c60a2d48cb8f55bf
Fynn Becker <info@fynn-becker.de>
2016-07-13 17:40:01 +0200

    Fix ESLint configuration

--- gulp/config.js
+++ gulp/config.js
@@ -21,61 +21,7 @@ module.exports = {
   // JavaScript
   js: {
     eslint: {
-      extends: 'eslint:recommended',
-      parserOptions: {
-        ecmaVersion: 6
-      },
-      envs: ['browser'],
-      rules: {
-        // Best Practices
-        'block-scoped-var': 'error',
-        'curly': 'error',
-        'eqeqeq': 'error',
-        'no-alert': 'error',
-        'no-empty-function': 'error',
-        'no-extend-native': 'error',
-        'no-floating-decimal': 'error',
-        'no-loop-func': 'error',
-        'no-native-reassign': 'error',
-        'no-self-compare': 'error',
-        'no-warning-comments': 'error',
-        'yoda': 'error',
-
-        // Variables
-        'no-shadow': 'error',
-        'no-shadow-restricted-names': 'error',
-        'no-use-before-define': 'error',
-
-        // Stylistic
-        'array-bracket-spacing': 'error',
-        'block-spacing': 'error',
-        'camelcase': 'error',
-        'comma-spacing': 'error',
-        'computed-property-spacing': 'error',
-        'key-spacing': 'error',
-        'keyword-spacing': 'error',
-        'no-bitwise': 'error',
-        'no-mixed-operators': 'error',
-        'no-spaced-func': 'error',
-        'no-trailing-spaces': 'error',
-        'no-unneeded-ternary': 'error',
-        'no-whitespace-before-property': 'error',
-        'object-property-newline': 'error',
-        'semi': 'error',
-        'semi-spacing': 'error',
-        'space-before-blocks': 'error',
-        'space-before-function-paren': ['error', 'never'],
-        'space-in-parens': 'error',
-        'space-infix-ops': 'error',
-        'space-unary-ops': 'error',
-
-        // ECMAScript 6
-        'arrow-spacing': 'error',
-        'no-const-assign': 'error',
-        'no-var': 'error',
-        'rest-spread-spacing': 'error',
-        'template-curly-spacing': 'error'
-      }
+      configFile: "./gulp/eslint.json"
     },
     babel: {
       presets: ['es2015'],
