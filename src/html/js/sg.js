9adf54ea59e96c28859e4bdd71195fdb75045aeb
Fynn Becker <info@fynn-becker.de>
2016-08-24 11:02:42 +0200

    Add basic accessibility testing methods

--- src/html/js/sg.js
+++ src/html/js/sg.js
@@ -93,3 +93,107 @@
     sgNavBtn[i].addEventListener('click', toggleSgMenuDropdown);
   }
 })();
+
+
+
+(function() {
+  let a11yButtons = document.querySelectorAll('.js-sg-a11y');
+
+
+
+  /* A11Y CONTRAST CECK
+   * ==================================================== */
+
+  let a11yContrast = {
+    active: false,
+    elementList: document.querySelectorAll('html'),
+    toggle: function(button) {
+      if (this.active) {
+        for (let i = 0; i < this.elementList.length; i++) {
+          this.elementList[i].style.WebkitFilter = null;
+          this.elementList[i].style.filter       = null;
+        }
+        button.classList.remove('is-active');
+        this.active = false;
+      } else {
+        for (let i = 0; i < this.elementList.length; i++) {
+          this.elementList[i].style.WebkitFilter = 'grayscale(100%)';
+          this.elementList[i].style.filter       = 'grayscale(100%)';
+        }
+        button.classList.add('is-active');
+        this.active = true;
+      }
+    }
+  };
+
+
+
+  /* A11Y HIDE ARIA-HIDDEN="true"
+   * ==================================================== */
+
+  let a11yAriaHidden = {
+    active: false,
+    elementList: document.querySelectorAll('[aria-hidden="true"]'),
+    toggle: function(button) {
+      if (this.active) {
+        for (let i = 0; i < this.elementList.length; i++) {
+          this.elementList[i].style.display = null;
+        }
+        button.classList.remove('is-active');
+        this.active = false;
+      } else {
+        for (let i = 0; i < this.elementList.length; i++) {
+          this.elementList[i].style.display = 'none';
+        }
+        button.classList.add('is-active');
+        this.active = true;
+      }
+    }
+  };
+
+
+
+  /* A11Y HIGHLIGHT MISSING ALT ATTRIBUTE
+   * ==================================================== */
+
+  let a11yAlt = {
+    active: false,
+    elementList: document.querySelectorAll('img:not([alt])'),
+    toggle: function(button) {
+      if (this.active) {
+        for (let i = 0; i < this.elementList.length; i++) {
+          this.elementList[i].style.outline = null;
+        }
+        button.classList.remove('is-active');
+        this.active = false;
+      } else {
+        for (let i = 0; i < this.elementList.length; i++) {
+          this.elementList[i].style.outline = '1em solid red';
+        }
+        button.classList.add('is-active');
+        this.active = true;
+      }
+    }
+  };
+
+
+
+  /* TOGGLE A11Y TESTS
+   * ==================================================== */
+
+  let toggleA11y = function(event) {
+    event.preventDefault();
+
+    if (this.classList.contains('js-sg-a11y-contrast')) {
+      a11yContrast.toggle(this);
+    } else if (this.classList.contains('js-sg-a11y-aria-hidden')) {
+      a11yAriaHidden.toggle(this);
+    } else if (this.classList.contains('js-sg-a11y-alt')) {
+      a11yAlt.toggle(this);
+    }
+  };
+
+  for (let i = 0; i < a11yButtons.length; i++) {
+    a11yButtons[i].addEventListener('click', toggleA11y);
+  }
+})();
