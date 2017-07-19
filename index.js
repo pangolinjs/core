a3c655bc330cd4d6182c79a5d5858dadf2799726
Fynn Becker <info@fynn-becker.de>
2017-07-19 11:48:25 +0200

    Remove check for local styleguide

--- index.js
+++ index.js
@@ -1,34 +1,8 @@
-'use strict'
-
-/* DEPENDENCIES
- * ========================================================================== */
-
-const chalk = require('chalk')
 const init = require('front-end-styleguide-init')
 const path = require('path')
 const spawn = require('child_process').spawn
 
-/* SEARCH LOCAL INSTALLATION
- * ========================================================================== */
-
-const searchLocalInstallation = dir => {
-  try {
-    require.resolve(`${dir}/node_modules/front-end-styleguide`)
-  } catch (error) {
-    console.error(`
-${chalk.black.bgRed('ERROR')} Local front-end-styleguide not found in ${chalk.magenta(dir)}
-
-Start a new project: front-end-styleguide init
-Install locally:     npm install front-end-styleguide --save-dev
-`)
-    process.exit(1)
-  }
-}
-
-/* UPDATE PROJECT
- * ========================================================================== */
-
-const updateProject = function (dir) {
+const updateProject = dir => {
   spawn('npm', ['update'], {
     cwd: dir,
     shell: true,
@@ -38,10 +12,7 @@ const updateProject = function (dir) {
   })
 }
 
-/* RUN GULP with optional ARGUMENTS
- * ========================================================================== */
-
-const spawnGulp = function (dir, args) {
+const spawnGulp = (dir, args) => {
   const gulpBin = path.resolve(require.resolve('gulp'), '../bin/gulp.js')
 
   spawn(`node "${gulpBin}"`, [...args, `--dir="${dir}"`], {
@@ -53,9 +24,6 @@ const spawnGulp = function (dir, args) {
   })
 }
 
-/* EXPOSE to OUTER SPACE
- * ========================================================================== */
-
 module.exports = function () {
   const processArgs = process.argv
   const args = processArgs.slice(2)
@@ -66,16 +34,10 @@ module.exports = function () {
       break
 
     case 'update':
-      if (!process.env.TEST) {
-        searchLocalInstallation(process.cwd())
-      }
       updateProject(process.cwd())
       break
 
     default:
-      if (!process.env.TEST) {
-        searchLocalInstallation(process.cwd())
-      }
       spawnGulp(process.cwd(), args)
       break
   }
