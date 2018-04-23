d01d4473abeb6c3a58b9ac54b5ef85df71a7ad7e
Fynn Becker <info@fynn-becker.de>
2018-04-23 07:29:36 +0200

    Add more flexibility to webpack project config

--- build/webpack.project.config.js
+++ build/webpack.project.config.js
@@ -4,8 +4,17 @@ module.exports = context => {
   const configPath = path.join(context, 'config/webpack.js')
 
   try {
-    const config = require(configPath)(context)[process.env.FESG_ENV]
-    return config || {}
+    const config = require(configPath)
+
+    if (typeof config.configure === 'function') {
+      return config.configure(context)
+    }
+
+    if (typeof config.configure === 'object') {
+      return config.configure
+    }
+
+    return {}
   } catch (error) {
     return {}
   }
