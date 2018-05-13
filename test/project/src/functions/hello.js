44268bd85a8fba1362a7d676f8c66b075a373e71
Fynn Becker <info@fynn-becker.de>
2018-05-13 11:54:52 +0200

    Add more JSDoc comments

--- test/project/src/functions/hello.js
+++ test/project/src/functions/hello.js
@@ -1,7 +1,7 @@
 /**
  * Say Hello to someone
  * @param {string} name Who do you want to greet?
- * @return {string} The greeting!
+ * @returns {string} The greeting!
  */
 export default function (name) {
   return `Hello ${name}!`
