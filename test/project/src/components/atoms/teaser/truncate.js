39b8d1d766b9efefebb95a57591f24dc31bd864d
Fynn Becker <info@fynn-becker.de>
2018-07-15 19:53:40 +0200

    Automate components

---
+++ test/project/src/components/atoms/teaser/truncate.js
@@ -0,0 +1,13 @@
+import truncate from 'lodash.truncate'
+
+/**
+ * Truncate text to 100 characters
+ * @param {string} text Text to truncate
+ * @returns {string} Truncated text
+ */
+export default function (text) {
+  return truncate(text, {
+    length: 100,
+    omission: '…'
+  })
+}
--- test/project/src/main.js
+++ test/project/src/main.js
@@ -1,6 +1,2 @@
-import './components'
-
-// Import custom colors
-if (process.env.PANGOLIN_ENV === 'dev' || process.env.PANGOLIN_ENV === 'build:dev') {
-  require('../config/branding.scss')
-}
+// Do global stuff
+console.log('Hello World!')
