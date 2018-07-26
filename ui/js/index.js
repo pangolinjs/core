ff8fa298130caa1e96f90481b2de3fd6de405668
Fynn Becker <info@fynn-becker.de>
2018-07-26 16:44:12 +0200

    Implement window reload

--- ui/js/index.js
+++ ui/js/index.js
@@ -1,1 +1,8 @@
-// Pangolin JavaScript
+// TODO: Search for free port
+const ws = new WebSocket('ws://localhost:12345')
+
+ws.onmessage = event => {
+  if (event.data === 'window-reload') {
+    window.location.reload()
+  }
+}
