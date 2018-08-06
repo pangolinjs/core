e6e805754318c99ae642a29253832a319026ef6c
Fynn Becker <info@fynn-becker.de>
2018-08-06 15:48:34 +0200

    Implement reconnecting WebSocket

--- ui/js/index.js
+++ ui/js/index.js
@@ -1,11 +1,53 @@
-/* global WebSocket, websocketPort */
+/* global WebSocket */
 
-if (websocketPort) {
-  const ws = new WebSocket(`ws://localhost:${websocketPort}`)
-
-  ws.onmessage = event => {
-    if (event.data === 'window-reload') {
-      window.location.reload()
-    }
+class Socket {
+  constructor (port) {
+    this.attempt = 1
+    this.port = port
+    this.timeout = null
+    this.ws = null
   }
+
+  /**
+   * Connect to WebSocket
+   */
+  connect () {
+    if (this.ws && this.ws.readyState !== WebSocket.CLOSED) {
+      return
+    }
+
+    if (this.timeout) {
+      return
+    }
+
+    this.ws = new WebSocket(`ws://localhost:${global.websocketPort}`)
+
+    this.ws.addEventListener('open', () => {
+      clearTimeout(this.timeout)
+      this.timeout = null
+      this.attempt = 1
+    })
+
+    this.ws.addEventListener('close', () => {
+      // Limited growth equation
+      const time = Math.floor(5000 - (5000 - 500) * Math.E ** (-0.05 * this.attempt))
+
+      this.timeout = setTimeout(() => {
+        this.attempt++
+        this.timeout = null
+        this.connect()
+      }, time)
+    })
+
+    this.ws.addEventListener('message', event => {
+      if (event.data === 'window-reload') {
+        window.location.reload()
+      }
+    })
+  }
+}
+
+if (global.websocketPort) {
+  const socket = new Socket(global.websocketPort)
+  socket.connect()
 }
