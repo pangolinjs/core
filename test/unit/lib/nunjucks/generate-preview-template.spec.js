3b1eeddda342416be260b9efec50e168accad548
Fynn Becker <info@fynn-becker.de>
2018-09-27 08:01:29 +0200

    Move Pangolin head to specific Nunjucks template block

--- test/unit/lib/nunjucks/generate-preview-template.spec.js
+++ test/unit/lib/nunjucks/generate-preview-template.spec.js
@@ -2,13 +2,61 @@ import generatePreviewTemplate from '../../../../lib/nunjucks/generate-preview-t
 import test from 'ava'
 
 test('generates default template', t => {
-  const template = generatePreviewTemplate('Hello World')
+  const template = generatePreviewTemplate({
+    head: '<link rel="stylesheet" href="main.css">',
+    body: 'Hello World'
+  })
 
   t.snapshot(template)
 })
 
 test('generates custom template', t => {
-  const template = generatePreviewTemplate('Hello World', 'test.njk')
+  const template = generatePreviewTemplate({
+    template: 'blog.njk',
+    head: '<link rel="stylesheet" href="main.css">',
+    body: 'Hello World'
+  })
 
   t.snapshot(template)
+})
+
+test('throws Error on missing options', t => {
+  const errorHead = t.throws(() => {
+    generatePreviewTemplate({ body: 'test' })
+  }, Error)
+
+  const errorBody = t.throws(() => {
+    generatePreviewTemplate({ head: 'test' })
+  }, Error)
+
+  t.is(errorHead.message, 'options.head missing')
+  t.is(errorBody.message, 'options.body missing')
+})
+
+test('throws TypeError on wrong option types', t => {
+  const errorTemplate = t.throws(() => {
+    generatePreviewTemplate({
+      template: 42,
+      head: 'test',
+      body: 'test'
+    })
+  }, TypeError)
+
+  const errorHead = t.throws(() => {
+    generatePreviewTemplate({
+      head: 1337,
+      body: 'test'
+    })
+  }, TypeError)
+
+  const errorBody = t.throws(() => {
+    generatePreviewTemplate({
+      head: 'test',
+      body: 3.142
+    })
+  }, TypeError)
+
+  t.is(errorTemplate.message, 'options.template must be of type string')
+  t.is(errorHead.message, 'options.head must be of type string')
+  t.is(errorBody.message, 'options.body must be of type string')
 })
