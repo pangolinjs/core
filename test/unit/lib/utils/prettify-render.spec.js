1e59ed8cef01181d5d178b65b4d27cd461cfc8d7
Fynn Becker <post@fynn.be>
2019-12-13 21:35:47 +0100

    feat: Prettify rendered HTML (Close #74)

---
+++ test/unit/lib/utils/prettify-render.spec.js
@@ -0,0 +1,13 @@
+import fs from 'fs'
+import path from 'path'
+import test from 'ava'
+
+import prettifyRender from '../../../../lib/utils/prettify-render'
+
+test('prettifies rendered HTML', t => {
+  const inputPath = path.join(__dirname, 'fixtures', 'ugly.html')
+  const input = fs.readFileSync(inputPath).toString()
+  const output = prettifyRender(input)
+
+  t.snapshot(output)
+})
