617f98f35659e9fa42a8da8bf4c65a00ab12a1a8
Fynn Becker <fynn@hey.com>
2020-09-07 10:40:41 +0200

    test: Update unit tests

--- test/unit/lib/generate-file-loader-options.spec.mjs
+++ test/unit/lib/generate-file-loader-options.spec.mjs
@@ -6,3 +6,8 @@ test('generates options', t => {
   const result = generateFileLoaderOptions({ type: 'js' })
   t.snapshot(result)
 })
+
+test('generates options with hash', t => {
+  const result = generateFileLoaderOptions({ type: 'js', hash: true })
+  t.snapshot(result)
+})
